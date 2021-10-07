7e6fa806326c6a3f72deb30b69af3ea7555582c5
Ranjith BR <ranjith@setu.co>
2021-10-07 22:47:22 +0530

    some styles and sync with JSON

--- src/options.js
+++ src/options.js
@@ -1,4 +1,11 @@
 // Saves options to storage.
+function saveStore(store) {
+  displaySync(store)
+  chrome.storage.sync.set({ 'urlalias': store });
+  // Update status to let user know options were saved.
+  toast("Saved")
+}
+
 function save() {
   var store = new Object();
 
@@ -6,60 +13,85 @@ function save() {
   var entries = $('#entries input');
   for (var i = 0; i < entries.length; i += 2) {
     key = entries[i].value;
-    val = entries[i+1].value;
+    val = entries[i + 1].value;
 
     if (key != '' && val != '') {
       store[key] = val;
     }
   }
-  chrome.storage.sync.set({'urlalias' : store});
-
-  // Update status to let user know options were saved.
-  $("#status").html("Changes saved").show().delay(3000).fadeOut();
+  saveStore(store)
 }
 
 // Restores select box state to saved value from localStorage.
 function onLoad() {
   // Restore all options
-  chrome.storage.sync.get('urlalias', function(store) {
+  chrome.storage.sync.get('urlalias', function (store) {
     store = store.urlalias;
-    for (var key in store) {
-      addRow(key, store[key]);
-    }
-    showJson()
+    displaySync(store)
+    showJson(store)
   });
 
   // Register click handlers
   $("#save").click(save);
-  $('#newEntry').click(function() { addRow("", ""); });
-  // $('#download').click(downloadConfig);
+  $('#newEntry').click(function () { addRow("", ""); });
+  $('#savejson').click(saveJson);
+
+  var elem = document.querySelector('.collapsible.expandable');
+  var instance = M.Collapsible.init(elem, {
+    accordion: false
+  });
+
 }
 
 // Add an individual row to the table.
 function addRow(alias, redirect) {
   var tr = $('<tr>');
 
-  var del = $('<button class="mdl-button mdl-js-button mdl-button--raised">Delete</button>');
-  del.click(function() { tr.remove();  });
+  var del = $('<button class="waves-effect waves-light btn-small pink">Delete</button>');
+  del.click(function () { tr.remove(); });
 
-  tr.append($('<td width="30%">').append($('<input>').val(alias)));
+  tr.append($('<td width="20%">').append($('<input>').val(alias)));
   tr.append($('<td width="60%">').append($('<input>').val(redirect)));
   tr.append($('<td width="10%">').append(del));
 
   $('#entries tbody').append(tr);
 }
 
-function showJson(){
-  chrome.storage.sync.get('urlalias', function(store) {
-    store = store.urlalias;
-    let data = []
-    for (var key in store) {
-      data.push({key: key, value:store[key]})
-    }
-    let txt = JSON.stringify(data, undefined, 2);
-    $("#json-data").val(txt)
-    M.textareaAutoResize($('#json-data'));
-  });
+function showJson(store) {
+  let data = {}
+  for (var key in store) {
+    data[key] = store[key]
+  }
+  let txt = JSON.stringify(data, undefined, 2);
+  $("#json-data").val(txt)
+  M.textareaAutoResize($('#json-data'));
+}
+
+function showKeyVals(store) {
+  $('#entries tbody').empty()
+  for (var key in store) {
+    addRow(key, store[key]);
+  }
+}
+
+function displaySync(store) {
+  showKeyVals(store)
+  showJson(store)
+}
+
+function saveJson() {
+  let jsonData = $("#json-data").val()
+  try {
+    let store = JSON.parse(jsonData)
+    saveStore(store)
+  }
+  catch (e) {
+    toast("invalid JSON")
+  }
+}
+
+function toast(msg) {
+  M.toast({ html: msg })
 }
 
 
